49f6a9b84337d58a845ff47b0d03319172a6c9ed
Anoob Anto K <anoob.anto.kodankandath@intel.com>
2023-02-08 11:17:25 +0530

    Fix license, copyright and file format
    Signed-off-by: Anoob Anto K <anoob.anto.kodankandath@intel.com>

--- services/faceMaskDetection/utils/nms.py
+++ services/faceMaskDetection/utils/nms.py
@@ -1,7 +1,21 @@
 #
-# Copyright (c) 2022 Intel Corporation
+# Copyright (C) 2020-2023 Intel Corporation
 #
-# -*- encoding=utf-8
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 import numpy as np
 
 def single_class_non_max_suppression(bboxes, confidences, conf_thresh=0.2, iou_thresh=0.5, keep_top_k=-1):
